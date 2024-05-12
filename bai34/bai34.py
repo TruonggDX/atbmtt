097fb471c05f4ce1033d8f7a71fb62d824201c1f
TruonggDX <dinhxuantruong2003coi@gmail.com>
2024-05-12 22:09:51 +0700

    bai34

--- bai34/bai34.py
+++ bai34/bai34.py
@@ -1,8 +1,11 @@
+#pip install requests
+#pip install beautifulsoup4
+
 import requests 
 from bs4 import BeautifulSoup 
  
 # URL của trang web cần kiểm tra 
-url = "Nhập trang web vào đây" 
+url = "https://www.facebook.com/" 
  
 # Các giá trị cần kiểm tra trong URL 
 payloads = ["'", "\"", "--", "#", "/*", "*/", ";"] 
@@ -18,3 +21,5 @@ for payload in payloads:
     # Kiểm tra xem trang web có hiển thị lỗi SQL hoặc thông báo lỗi không 
     if "sql syntax" in soup.text.lower() or "syntax error" in soup.text.lower() or "mysql_fetch_array()" in soup.text.lower(): 
         print("Website có thể bị lỗ hổng SQL Injection với payload: " + payload)
+    else:
+        print("website không có lỗi")
