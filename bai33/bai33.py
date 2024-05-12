26881c2d9c228d5e2a1720fc23d35be789f6ba68
RPTSimpLe <simplee2083@gmail.com>
2024-05-12 23:06:24 +0700

    abc def jqk

--- bai33/bai33.py
+++ bai33/bai33.py
@@ -1,14 +1,24 @@
+#lệnh cài thư viện nếu chưa có: pip install mysql-connector-python
+# create database example_db;
+# use example_db;
+# create table test(
+# 	id varchar(100) primary key,
+#     vidu varchar(100)
+# );
+# insert into test(id,vidu) values (";","1"),("minh","vu");
+
+# select * FROM test WHERE id = ";"
 import mysql.connector 
  
 # Thông tin kết nối đến cơ sở dữ liệu 
 host = "localhost" 
 user = "root" 
-password = "28042003" 
-database = "db_hoadon"
-port=3307
+password = "td082003" 
+database = "example_db"
+port=3306
  
 # Các giá trị cần kiểm tra trong các truy vấn SQL 
-payloads =["'", "\"", "--", "#", "/*", "*/", ";"] 
+payloads =[ "\"", "--", "#", "/*", "*/", ";"] 
  
 # Kết nối đến cơ sở dữ liệu 
 conn = mysql.connector.connect(host=host, user=user, password=password, database=database,port=port) 
@@ -23,7 +33,7 @@ for table in tables:
     print("Kiểm tra lỗ hổng SQL Injection trên bảng:", table) 
     # Thực thi các truy vấn SQL với các giá trị kiểm tra 
     for payload in payloads: 
-        query = f"SELECT * FROM {table} WHERE column_name = '{payload}'" 
+        query = f"SELECT * FROM {table} WHERE id = '{payload}'" 
         cursor.execute(query) 
  
         # Kiểm tra xem truy vấn SQL có trả về kết quả hay không 
